3f49f41da287d02669817257d398293363c4d51b
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-03-04 14:21:11 -0300

    feat(run): Integrate APIs and verify connectivity to the Google Sheetpython3

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
-print('Hello world!')
+# Import libraries
+import gspread
+from google.oauth2.service_account import Credentials
+import random
+
+# Scopes for accessing credentials from Google Cloud    
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# Scopes for accessing credentials from Google Cloud
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD.open("PythonQuizDatabase")
+
+# setting a variable to a specific google worksheet
+questions = SHEET.worksheet('questions')
+history = SHEET.worksheet('history')
+
+print(questions)
+print(history)
